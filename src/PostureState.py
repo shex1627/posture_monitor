1bfe79f718561001159e7983a2890dd2cc0223b2
shex1627 <alistar.huang@gmail.com>
2022-01-06 08:30:21 -0800

    finishing phase0.5

---
+++ src/PostureState.py
@@ -0,0 +1,111 @@
+#import cv2
+#import mediapipe as mp
+import logging
+import sys
+from collections import OrderedDict
+import time 
+import os
+from typing import Callable
+import pickle
+import json
+import numpy as np
+import threading
+import winsound
+import logging
+
+
+logger = logging.getLogger("PostureState")
+
+class OrderedDefaultDict(OrderedDict):
+    factory = list
+
+    def __missing__(self, key):
+        self[key] = value = self.factory()
+        return value
+
+
+class PostureState:
+    fps = 25
+    EXPORT_MIN = 10
+    SECOND_PER_MIN = 60
+    FRAME_THRESHOLD = fps * SECOND_PER_MIN * EXPORT_MIN
+
+    def __init__(self, score_func: Callable, if_bad_posture: Callable, dir=None):
+        self.score_by_frame = OrderedDefaultDict()
+        self.score_by_second = OrderedDefaultDict()
+        self.score_lst = []
+        self.score_func = score_func
+        self.if_bad_posture = if_bad_posture
+        self.dir = dir
+        self.last_alert_time = 0
+
+    def update(self, landmarks: list):
+        """ add postuer data per frame. """
+        time_hash = self.get_time()
+        score = self.score_func(landmarks)
+        self.score_lst.append(score)
+        self.score_by_frame[time_hash].append(score)
+        if self.dir and len(self.score_lst) > PostureState.FRAME_THRESHOLD:
+            # outfile_name = os.path.join(self.dir, f"data_{self.get_time()}.pkl")
+            # logger.info(f"saving data to {outfile_name}")
+            # with open(outfile_name, 'wb') as outfile:
+            #     pickle.dump(self.score_lst, outfile)
+            # exporting frame data
+            outfile_name = os.path.join(self.dir, f"data_{self.get_time()}.json")
+            logger.info(f"saving data to {outfile_name}")
+            with open(outfile_name, 'w') as outfile:
+                json.dump(self.score_by_frame, outfile, indent=4)
+
+            self.score_lst = []
+            self.score_by_frame = OrderedDefaultDict()
+        if len(self.score_lst) > PostureState.fps:
+            data_window = self.score_lst[-PostureState.fps::]
+            data_window = list(map(lambda score: np.round(score, 2), data_window))
+            mean_score = np.mean(list(map(self.if_bad_posture, data_window)))
+
+    def get_time(self):
+        return int(time.time())
+
+    def bad_posture_frame(self):
+        "return if last second has bad posture"
+        if self.score_lst:
+            return self.if_bad_posture(self.score_lst[-1])
+
+    def bad_posture_second(self):
+        "return if last second has bad posture"
+        if len(self.score_lst) > PostureState.fps:
+            data_window = self.score_lst[-PostureState.fps::]
+            data_window = list(map(lambda score: np.round(score, 2), data_window))
+            mean_score = np.mean(list(map(self.if_bad_posture, data_window)))
+            return mean_score > 0.9
+        return False
+
+    def bad_posture(self):
+        """ check if posture is bad in last X seconds"""
+        if_in_bad_posture = False
+        #logging.info(len(self.score_lst))
+        seconds_threshold = 15
+        frame_offset = seconds_threshold * PostureState.fps
+        percent_threshold = 0.9
+        if len(self.score_lst) >= frame_offset:
+            #logging.info("check if posture is bad")
+            data_window = self.score_lst[-frame_offset::]
+            mean_score = np.mean(list(map(self.if_bad_posture, data_window)))
+            logging.info(f"mean score {mean_score} in {seconds_threshold} seconds")
+            if_in_bad_posture = mean_score > percent_threshold
+            #logging.info(f"bad posture: {if_in_bad_posture}")
+        return if_in_bad_posture
+            
+    
+    def check_posture(self, alert_sound_file="./resources/alert.wav"):
+        # relative import is bad
+        # make sure alert don't spam
+        in_bad_posture = self.bad_posture()
+        logging.info(f"in bad posture: {in_bad_posture}")
+        if in_bad_posture and self.bad_posture_second():
+            logging.info("in bad posture, triggering alert")
+            now = self.get_time()
+            if now - self.last_alert_time > 0.5:
+                self.last_alert_time = now 
+                winsound.PlaySound(alert_sound_file, winsound.SND_ASYNC |  winsound.SND_FILENAME) #
+            
--- track_landmark.py
+++ track_landmark.py
@@ -41,6 +41,19 @@ with mp_pose.Pose(
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = pose.process(image)
 
+
+            
+
+        # Draw the pose annotation on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        mp_drawing.draw_landmarks(
+            image,
+            results.pose_landmarks,
+            mp_pose.POSE_CONNECTIONS,
+            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+        # 
+        image_flip = cv2.flip(image, 1)
         # log landmarks
         if results.pose_landmarks:
             landmark_lst = results.pose_landmarks.ListFields()[0][1]
@@ -61,24 +74,13 @@ with mp_pose.Pose(
             logging.info(f"lip_shoulder_y_diff: {lip_shoulder_y_diff}") #probably 0.1
             logging.info(f"left_lip_shoulder_x_diff: {left_lip_shoulder_x_diff}")
             logging.info(f"right_lip_shoulder_x_diff: {right_lip_shoulder_x_diff}")
+            y_increment = 40
+            y_init = 300
+            font_scale = 0.8
             
-
-        # Draw the pose annotation on the image.
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        mp_drawing.draw_landmarks(
-            image,
-            results.pose_landmarks,
-            mp_pose.POSE_CONNECTIONS,
-            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-        # 
-        image_flip = cv2.flip(image, 1)
-        y_increment = 40
-        y_init = 300
-        font_scale = 1
-        cv2.putText(image_flip, f"lip_shoulder_y_diff: {np.round(lip_shoulder_y_diff, 2)}", (10,y_init), font, font_scale, (0, 255, 0), 2, cv2.LINE_AA)
-        cv2.putText(image_flip, f"left_lip_shoulder_x_diff: {np.round(left_lip_shoulder_x_diff, 2)}", (10,y_init+y_increment*1), font, font_scale, (0, 255, 0), 2, cv2.LINE_AA)
-        cv2.putText(image_flip, f"right_lip_shoulder_x_diff: {np.round(right_lip_shoulder_x_diff, 2)}", (10,y_init+y_increment*2), font, font_scale, (0, 255, 0), 2, cv2.LINE_AA)
+            #cv2.putText(image_flip, f"lip_shoulder_y_diff: {np.round(lip_shoulder_y_diff, 2)}", (10,y_init), font, font_scale, (0, 255, 0), 2, cv2.LINE_AA)
+            #cv2.putText(image_flip, f"left_lip_shoulder_x_diff: {np.round(left_lip_shoulder_x_diff, 2)}", (10,y_init+y_increment*1), font, font_scale, (0, 255, 0), 2, cv2.LINE_AA)
+            #cv2.putText(image_flip, f"right_lip_shoulder_x_diff: {np.round(right_lip_shoulder_x_diff, 2)}", (10,y_init+y_increment*2), font, font_scale, (0, 255, 0), 2, cv2.LINE_AA)
         # Flip the image horizontally for a selfie-view display.
         cv2.imshow('MediaPipe Pose', image_flip)
         # leave app when click `esc` key
