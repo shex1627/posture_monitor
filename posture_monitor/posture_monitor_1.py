1a137a0f2fb9d5bdc91a740ce891e9da6f97d5fa
shex1627 <alistar.huang@gmail.com>
2022-01-14 23:35:11 -0800

    fix bug and add more metrics

--- posture_monitor/posture_monitor_1.py
+++ posture_monitor/posture_monitor_1.py
@@ -85,9 +85,18 @@ def main():
     
     headdown_10_seconds_metric_funct = if_metric_fail_avg_and_last_second("headdown", 
     lambda head_level: head_level > 0.31, seconds=10, percent=1.00)
-    headdown_10_seconds = PostureSubMetricTs("headdown_seconds", headdown_10_seconds_metric_funct,
-    "headdown_seconds")
+    headdown_10_seconds = PostureSubMetricTs("headdown_seconds", headdown_10_seconds_metric_funct)
     headdown_alert = PostureKDeltaAlert('headdown_10_seconds', headdown_10_seconds, 1)
+
+    rightshoulder_down_metric_funct = if_metric_fail_avg_and_last_second("right_shoulder_y", 
+    lambda right_shoulder_y: right_shoulder_y >= 0.57, seconds=2, percent=1.00)
+    rightshoulder_down_10_seconds = PostureSubMetricTs("rightshoulder_down_seconds", rightshoulder_down_metric_funct)    
+    rightshoulder_down_alert = PostureKDeltaAlert('rightshoulder_10s_down', rightshoulder_down_10_seconds, 1)
+
+    left_right_shoulder_y_diff = PostureMetricTs("left_right_shoulder_y_diff", metric_func=lambda landmarks: np.abs(landmark_lst[11].y - landmark_lst[12].y))
+    shoulder_tilt_metric_funct = if_metric_fail_avg_and_last_second("left_right_shoulder_y_diff", lambda level_diff: level_diff > 0.02, seconds=3, percent=1.00)
+    shoulder_tilt_seconds = PostureSubMetricTs("shoulder_tilt_seconds", shoulder_tilt_metric_funct)
+    shoulder_tilt_alert = PostureKDeltaAlert("shoulder_tilt_seconds", shoulder_tilt_seconds, 1)
 
     metricTsDict = {    
         headdown_metric.name: headdown_metric,
@@ -95,9 +104,12 @@ def main():
         left_shoulder_y_metric.name: left_shoulder_y_metric,
         right_shoulder_y_metric.name: right_shoulder_y_metric,
         left_shoulder_x_metric.name: left_shoulder_x_metric,
-        right_shoulder_x_metric.name: right_shoulder_x_metric
+        right_shoulder_x_metric.name: right_shoulder_x_metric,
+        rightshoulder_down_10_seconds.name: rightshoulder_down_10_seconds,
+        left_right_shoulder_y_diff.name: left_right_shoulder_y_diff,
+        shoulder_tilt_seconds.name: shoulder_tilt_seconds
     }
-    pSession = PostureSession(metricTsDict, [headdown_alert], data_dir="test_dir") #AlertRule(alert_rule=lambda metricDict: 
+    pSession = PostureSession(metricTsDict, [headdown_alert, shoulder_tilt_alert], data_dir="test_dir") #AlertRule(alert_rule=lambda metricDict: #headdown_alert, 
 
     if program_on:
         with Listener(on_press=on_press_start) as listener:
